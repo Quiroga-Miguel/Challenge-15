a89836db24604ac578a03a3baa2cfb919bf2b4b9
Quiroga Miguel <miguel.quirog@gmail.com>
2023-03-29 23:46:18 -0300

    consultar

--- Js/Main.js
+++ Js/Main.js
@@ -6,11 +6,30 @@
 
 // investigar a través de que método podemos quitar los guiones y reemplazar por espacios.
 
+//Ejercicio 1
 let num1 = parseInt(prompt("Ingrese primer número"));
 let num2 = parseInt(prompt("Ingrese segundo número"));
 
 let ResultadoResta = num1 - num2;
 let ResultadoMultiplicacion = num1 * num2;
-document.write(`<h2>El primer numero es ${num1}, el segundo numero es ${num2}<br></h2>`);
-document.write(`<h3> La resta de los numeros ingresado es ${ResultadoResta}<br></h3>`);
-document.write(`<h3>Multiplicación de los numeros ingresado es ${ResultadoMultiplicacion} </h3> <br>`);
+document.write(`<h2>El primer numero es ${num1}, el segundo numero es ${num2}.<br></h2>`);
+document.write(`<h3>La resta de los numeros ingresado es ${ResultadoResta}</h3>.`);
+document.write(`<h3>Multiplicación de los numeros ingresado es ${ResultadoMultiplicacion}.<br></h3>`);
+
+//Ejercicio 2
+let apellidoYnombre = prompt(`Ingrese tu nombres y apellido `);
+document.write(`<br><br>el nombre y apellido es ${apellidoYnombre} `);
+if(apellidoYnombre.includes(`a`) && apellidoYnombre.includes(`m`)){
+    console.log(`El nombres y apellido posee letra a y m `);
+    document.write(`<br>El nombres y apellido posee letra a y m `)
+} else{
+    console.log(`El nombres NO posee letra a y m`);
+    document.write(`<br>El nombres NO posee letra a y m`)
+}
+// console.log(`el apellido posee letra a o m `, apellido.includes(`a`) || apellido.includes(`m`));
+// console.log(`el nombre posee letra a o m `, nombre.includes(`a`) || nombre.includes(`m`));
+
+//Ejercicio 3 
+let pablitoGuiones = ('<br><br>Pablito-clavo-un-clavito');
+let pablitoEspcaio = pablitoGuiones.replace(/-/g,' ');
+document.write(pablitoEspcaio);
